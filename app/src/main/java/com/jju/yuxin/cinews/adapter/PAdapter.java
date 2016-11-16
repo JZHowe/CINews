1ec2ddb8cc43f01fbeb76554d3438a098ae977bb
KSuperLiu <superliu1995@gmail.com>
2016-11-16 14:29:23 +0800

    底部第二个图片获取

---
+++ app/src/main/java/com/jju/yuxin/cinews/adapter/PAdapter.java
@@ -0,0 +1,69 @@
+package com.jju.yuxin.cinews.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.jju.yuxin.cinews.R;
+import com.jju.yuxin.cinews.bean.NewsBean;
+import com.jju.yuxin.cinews.volleyutils.ImageCacheManager;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/11/16.
+ */
+
+public class PAdapter extends BaseAdapter {
+    private List<NewsBean> olist;
+    private Context context;
+    private LayoutInflater inflater;
+
+    public PAdapter(Context context,List<NewsBean> olist ) {
+        this.olist = olist;
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return olist.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return olist.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (view == null) {
+            viewHolder = new ViewHolder();
+            view = inflater.inflate(R.layout.picuture_item, null);
+            viewHolder.imageView = (ImageView)view.findViewById(R.id.p_imageView);
+            viewHolder.textView = (TextView)view.findViewById(R.id.p_textView);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        viewHolder.textView.setText(olist.get(position).getName());
+        ImageCacheManager.loadImage(context, olist.get(position).getUrl(), viewHolder.imageView, R.drawable.loading, R.drawable.failu, 0, 0, ImageView.ScaleType.FIT_XY);
+        return view;
+    }
+
+    class ViewHolder {
+        ImageView imageView;
+        TextView textView;
+    }
+
+}
