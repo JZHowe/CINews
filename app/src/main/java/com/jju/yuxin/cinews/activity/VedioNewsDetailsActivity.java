5af42bb532823f5c6d0a26ba029f710441a7be0d
huyuxin <851313063@qq.com>
2016-11-23 14:41:11 +0800

    Simon:完善了视频的全屏播放功能

--- app/src/main/java/com/jju/yuxin/cinews/activity/VedioNewsDetailsActivity.java
+++ app/src/main/java/com/jju/yuxin/cinews/activity/VedioNewsDetailsActivity.java
@@ -4,6 +4,7 @@ import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.Image;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -18,6 +19,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.MediaController;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
@@ -86,7 +88,7 @@ public class VedioNewsDetailsActivity extends BaseActivity {
                 case SUCCESS_LOAD:
                     vedioinfos = (List<VedioInfoBean>) msg.obj;
                     //将内容填充到ListView
-                    lv_more_vedio.setAdapter(new VedioList_Adapter(VedioNewsDetailsActivity.this, vedioinfos,volleyUtils));
+                    lv_more_vedio.setAdapter(new VedioList_Adapter(VedioNewsDetailsActivity.this, vedioinfos, volleyUtils));
                     break;
                 //视频新闻加载成功
                 case SUCCESS_LOAD_DETAIL:
@@ -118,6 +120,9 @@ public class VedioNewsDetailsActivity extends BaseActivity {
     private ListView lv_more_vedio;
     private ImageView iv_download;
     private DownloadManager downManager;
+    private RelativeLayout rl_vv;
+    private LandScapeVedio landScapeVedio;
+    private getPositionBroadcast positionBroadcast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -135,7 +140,7 @@ public class VedioNewsDetailsActivity extends BaseActivity {
         SharedClickListener sharedClickListener = new SharedClickListener();
 
 
-        downManager = (DownloadManager)getSystemService(Context.DOWNLOAD_SERVICE);
+        downManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
 
         //分享按钮
         iv_shard = (ImageView) findViewById(R.id.iv_shard);
@@ -152,10 +157,19 @@ public class VedioNewsDetailsActivity extends BaseActivity {
         //视屏播放器
         vedio_paly = (VideoView) findViewById(R.id.vedio_paly);
 
+        //视屏播放器上面嵌套的布局
+        rl_vv = (RelativeLayout) findViewById(R.id.rl_vv);
+
         //更多视频
         lv_more_vedio = (ListView) findViewById(R.id.lv_more_vedio);
 
         iv_download = (ImageView) findViewById(R.id.iv_download);
+
+        //注册一个广播用来接收播放位置
+        positionBroadcast = new getPositionBroadcast();
+        IntentFilter filter=new IntentFilter();
+        filter.addAction("sendposition");
+        registerReceiver(positionBroadcast, filter);
 
         //下载视频到本地
         iv_download.setOnClickListener(new View.OnClickListener() {
@@ -166,10 +180,7 @@ public class VedioNewsDetailsActivity extends BaseActivity {
             }
         });
 
-
-
-        JsoupUtils.getNewPaper(path,mhandler);
-
+        JsoupUtils.getNewPaper(path, mhandler);
 
         lv_more_vedio.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -177,12 +188,13 @@ public class VedioNewsDetailsActivity extends BaseActivity {
                 //获取当前点击的item的对象
                 VedioInfoBean vedioInfoBean = vedioinfos.get(position);
                 //跳转至新闻详情页面
-                Intent intent=new Intent(VedioNewsDetailsActivity.this,VedioNewsDetailsActivity.class);
-                intent.putExtra("vedio_news",vedioInfoBean);
+                Intent intent = new Intent(VedioNewsDetailsActivity.this, VedioNewsDetailsActivity.class);
+                intent.putExtra("vedio_news", vedioInfoBean);
                 startActivity(intent);
                 finish();
             }
         });
+
 
     }
 
@@ -213,13 +225,13 @@ public class VedioNewsDetailsActivity extends BaseActivity {
                     Toast.makeText(VedioNewsDetailsActivity.this, "已取消", Toast.LENGTH_SHORT).show();
                 } else {
                     String loginUserid = LoginPlatformUtil.getLoginUserid();
-                    if (loginUserid!=null) {
+                    if (loginUserid != null) {
                         mFavorBean.setUserid(loginUserid);
                         bt_top_right.setBackgroundResource(R.drawable.shoucang_new_two);
                         DbUtils.saveFavor(mFavorBean);
                         isFavor = true;
                         Toast.makeText(VedioNewsDetailsActivity.this, "已收藏", Toast.LENGTH_SHORT).show();
-                    }else{
+                    } else {
                         Toast.makeText(VedioNewsDetailsActivity.this, "请先登录!", Toast.LENGTH_SHORT).show();
                         login_();
                     }
@@ -236,11 +248,28 @@ public class VedioNewsDetailsActivity extends BaseActivity {
             }
             //如果已经播放过那么跳转到上次的播放进度之后
         } else {
-            vedio_paly.seekTo(per_position);
-            vedio_paly.start();
-            per_position = 0;
+            if (!vedio_paly.isPlaying()){
+                vedio_paly.seekTo(per_position);
+                vedio_paly.start();
+            }
         }
+
+        //给最外层设置长按事件
+        rl_vv.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                int currentPosition = vedio_paly.getCurrentPosition();
+                Intent fullintent = new Intent(VedioNewsDetailsActivity.this, LandScapeVedio.class);
+                fullintent.putExtra("currentPosition", currentPosition);
+                fullintent.putExtra("play_src", vedioInfoBean.getPlay_src());
+                startActivity(fullintent);
+                return false;
+            }
+        });
     }
+
+
+
 
     //接收list的信息
     private void getInfo() {
@@ -249,7 +278,7 @@ public class VedioNewsDetailsActivity extends BaseActivity {
         getFavor();
         //只有登陆了用户才要判断当前内容是否已经被收藏
         String loginUserid = LoginPlatformUtil.getLoginUserid();
-        if (loginUserid!=null) {
+        if (loginUserid != null) {
             //判断是否已经收藏
             if (DbUtils.searchVideoFavor(vedioInfoBean).size() > 0) {
                 isFavor = true;
@@ -274,6 +303,8 @@ public class VedioNewsDetailsActivity extends BaseActivity {
     @Override
     protected void onPause() {
         super.onPause();
+
+
         e(TAG, "onPause" + "");
         //当切换到分享界面，需要暂停当前的的视频播放
         //记录下当前播放的进度
@@ -289,9 +320,11 @@ public class VedioNewsDetailsActivity extends BaseActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(vedio_paly!=null){
+        if (vedio_paly != null) {
             vedio_paly.stopPlayback(); //将VideoView所占用的资源释放掉
         }
+
+        unregisterReceiver(positionBroadcast);
 
         e(TAG, "onDestroy" + "");
     }
@@ -363,7 +396,6 @@ public class VedioNewsDetailsActivity extends BaseActivity {
                 //分享按钮
                 case R.id.iv_shard:
                     ShareMessage();
-
                     break;
                 default:
                     break;
@@ -380,6 +412,8 @@ public class VedioNewsDetailsActivity extends BaseActivity {
         @Override
         public void onCompletion(MediaPlayer mp) {
             Toast.makeText(VedioNewsDetailsActivity.this, "播放完成.", Toast.LENGTH_SHORT).show();
+            vedio_paly.seekTo(0);
+            vedio_paly.start();
         }
     }
 
@@ -394,85 +428,86 @@ public class VedioNewsDetailsActivity extends BaseActivity {
 
             hlog.e(platform.getName());
             //微信分享
-            if (Wechat.NAME.equals(platform .getName())){
+            if (Wechat.NAME.equals(platform.getName())) {
 
                 // text是分享文本，所有平台都需要这个字段
-                paramsToShare.setText(vedioInfoBean.getNews_info()+""+vedioInfoBean.getPlay_src());
+                paramsToShare.setText(vedioInfoBean.getNews_info() + "" + vedioInfoBean.getPlay_src());
                 //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
-                paramsToShare.setImageUrl(vedioInfoBean.getImg_src()+"");
+                paramsToShare.setImageUrl(vedioInfoBean.getImg_src() + "");
                 // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
                 //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
                 // url仅在微信（包括好友和朋友圈）中使用
-                paramsToShare.setUrl(vedioInfoBean.getPlay_src()+"");
+                paramsToShare.setUrl(vedioInfoBean.getPlay_src() + "");
 
                 //QQ分享
-            }else if(QQ.NAME.equals(platform .getName())){
+            } else if (QQ.NAME.equals(platform.getName())) {
                 //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
                 // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
                 paramsToShare.setTitle(vedioInfoBean.getNews_title());
                 // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-                paramsToShare.setTitleUrl(vedioInfoBean.getPlay_src()+"");
+                paramsToShare.setTitleUrl(vedioInfoBean.getPlay_src() + "");
                 // text是分享文本，所有平台都需要这个字段
-                paramsToShare.setText(vedioInfoBean.getNews_info()+""+vedioInfoBean.getPlay_src());
+                paramsToShare.setText(vedioInfoBean.getNews_info() + "" + vedioInfoBean.getPlay_src());
                 //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
-                paramsToShare.setImageUrl(vedioInfoBean.getImg_src()+"");
+                paramsToShare.setImageUrl(vedioInfoBean.getImg_src() + "");
 
                 // comment是我对这条分享的评论，仅在人人网和QQ空间使用
                 paramsToShare.setComment(vedioInfoBean.getPlay_count());
                 // site是分享此内容的网站名称，仅在QQ空间使用
                 paramsToShare.setSite("CINews");
                 // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-                paramsToShare.setSiteUrl(vedioInfoBean.getPlay_src()+"");
+                paramsToShare.setSiteUrl(vedioInfoBean.getPlay_src() + "");
 
                 //新浪微博分享
-            } else if(SinaWeibo.NAME.equals(platform .getName())){
+            } else if (SinaWeibo.NAME.equals(platform.getName())) {
 
                 // text是分享文本，所有平台都需要这个字段
-                paramsToShare.setText(vedioInfoBean.getNews_info()+""+vedioInfoBean.getPlay_src());
+                paramsToShare.setText(vedioInfoBean.getNews_info() + "" + vedioInfoBean.getPlay_src());
                 //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
-                paramsToShare.setImageUrl(vedioInfoBean.getImg_src()+"");
+                paramsToShare.setImageUrl(vedioInfoBean.getImg_src() + "");
 
                 //微信朋友圈分享
-            } else if(WechatMoments.NAME.equals(platform.getName())){
+            } else if (WechatMoments.NAME.equals(platform.getName())) {
 
                 paramsToShare.setTitle(vedioInfoBean.getNews_title());
-                paramsToShare.setText(vedioInfoBean.getNews_info()+""+vedioInfoBean.getPlay_src());
+                paramsToShare.setText(vedioInfoBean.getNews_info() + "" + vedioInfoBean.getPlay_src());
                 paramsToShare.setShareType(Platform.SHARE_WEBPAGE);
-                paramsToShare.setUrl(vedioInfoBean.getVideo_src()+"");
-                paramsToShare.setImagePath(vedioInfoBean.getImg_src()+"");
+                paramsToShare.setUrl(vedioInfoBean.getVideo_src() + "");
+                paramsToShare.setImagePath(vedioInfoBean.getImg_src() + "");
 
                 //微信收藏
-            }else if(WechatFavorite.NAME.equals(platform.getName())){
+            } else if (WechatFavorite.NAME.equals(platform.getName())) {
                 paramsToShare.setTitle(vedioInfoBean.getNews_title());
-                paramsToShare.setText(vedioInfoBean.getNews_info()+""+vedioInfoBean.getPlay_src());
+                paramsToShare.setText(vedioInfoBean.getNews_info() + "" + vedioInfoBean.getPlay_src());
                 paramsToShare.setShareType(Platform.SHARE_WEBPAGE);
-                paramsToShare.setUrl(vedioInfoBean.getVideo_src()+"");
-                paramsToShare.setImagePath(vedioInfoBean.getImg_src()+"");
+                paramsToShare.setUrl(vedioInfoBean.getVideo_src() + "");
+                paramsToShare.setImagePath(vedioInfoBean.getImg_src() + "");
             }
         }
     }
 
     /**
      * 下载视频到本地
+     *
      * @param play_info
      */
     private void downloadVedio(VedioInfoBean play_info) {
         //判断是否存在SD卡
-        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
-            File file=new File(Environment.DIRECTORY_DOWNLOADS, play_info.getNews_title().replace(" ","")+".mp4");
-            Log.e(TAG, "downloadVedio" + file.getAbsolutePath()+file.exists());
-            if (!file.exists()){
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            File file = new File(Environment.DIRECTORY_DOWNLOADS, play_info.getNews_title().replace(" ", "") + ".mp4");
+            e(TAG, "downloadVedio" + file.getAbsolutePath() + file.exists());
+            if (!file.exists()) {
                 DownloadManager.Request request = new DownloadManager.Request(Uri.parse(play_info.getPlay_src()));
                 request.setNotificationVisibility(VISIBILITY_VISIBLE);//用于设置下载时时候在状态栏显示通知信
                 request.allowScanningByMediaScanner();//用于设置是否允许本MediaScanner扫描。
                 request.setTitle("下载中");
                 request.setDescription("视频正在下载中");
-                request.setDestinationInExternalFilesDir(this, Environment.DIRECTORY_DOWNLOADS, play_info.getNews_title().replace(" ","")+".mp4");
+                request.setDestinationInExternalFilesDir(this, Environment.DIRECTORY_DOWNLOADS, play_info.getNews_title().replace(" ", "") + ".mp4");
                 long enqueue = downManager.enqueue(request);
-            }else{
+            } else {
                 Toast.makeText(this, "您已经下载过了这个视频...", Toast.LENGTH_SHORT).show();
             }
-        }else{
+        } else {
             Toast.makeText(this, "请插入SD卡...", Toast.LENGTH_SHORT).show();
         }
     }
@@ -495,6 +530,7 @@ public class VedioNewsDetailsActivity extends BaseActivity {
             startActivityForResult(new Intent(VedioNewsDetailsActivity.this, LoginActivity.class), REQUEST_CODE);
         }
     }
+
     //根据登录成功的返回信息更新数据库信息
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -528,6 +564,23 @@ public class VedioNewsDetailsActivity extends BaseActivity {
                 DbUtils.saveUser(user);
             }
             e(TAG, "onActivityResult" + "platform:" + platform + "userinfo" + userinfo);
+        }
+    }
+
+    public class getPositionBroadcast extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            int position = intent.getIntExtra("position", 0);
+            e(TAG, "onReceive" + "position"+position);
+            vedio_paly.seekTo(position);
+                if (position==0){
+                    per_position=0; 
+                    vedio_paly.seekTo(vedio_paly.getDuration()-1);
+                }else{
+                    vedio_paly.start();
+                }
         }
     }
 
